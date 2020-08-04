b71031f23b78efac602b3c92ccdc4b015d377a9e
AlmightyOdin <65109915+AlmightyOdin@users.noreply.github.com>
2020-08-04 19:04:18 +0530

    Added Spiky Hammer(Spiker Drop)

---
+++ Items/projectiles/SpikyHammerProj.cs
@@ -0,0 +1,54 @@
+﻿using Terraria.ModLoader;
+using Terraria;
+using Terraria.ID;
+using Microsoft.Xna.Framework;
+namespace RaidersMod.Items.projectiles
+{
+    class SpikyHammerProj : ModProjectile
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Spiky Hammer");
+        }
+        Vector2 DirectionToGo = Vector2.Zero;
+        public override void SetDefaults()
+        {
+            projectile.aiStyle = -1;
+            projectile.damage = 36;
+            projectile.knockBack = 1.6f;
+            projectile.penetrate = -1;
+            projectile.friendly = true;
+            projectile.melee = true;
+            projectile.width = projectile.height = 25;
+        }
+        bool ShouldReturn = false;
+        public override void OnHitNPC(NPC target, int damage, float knockback, bool crit)
+        {
+            ShouldReturn = true;
+            projectile.tileCollide = false;
+        }
+        public override bool OnTileCollide(Vector2 oldVelocity)
+        {
+            ShouldReturn = true;
+            projectile.tileCollide = false;
+            Main.PlaySound(SoundID.Dig, projectile.position);
+            return false;
+        }
+        int returnTimer = 0;
+        public override void AI()
+        {
+            if(DirectionToGo == Vector2.Zero) DirectionToGo = Vector2.Normalize(Main.MouseWorld - projectile.Center);
+            Player player = Main.player[projectile.owner];
+            projectile.rotation += 0.38f * (float)projectile.direction;
+            if (Vector2.Distance(projectile.Center, player.Center) > 500 || ++returnTimer >= 60 || !player.channel || Vector2.Distance(projectile.Center,DirectionToGo) < 10) ShouldReturn = true;
+            if(!ShouldReturn)
+            {
+                projectile.velocity = DirectionToGo * 26f;
+            } else
+            {
+                projectile.velocity = Vector2.Normalize(player.Center - projectile.Center) * 28f;
+                if (projectile.Hitbox.Intersects(player.Hitbox)) projectile.Kill();
+            }
+        }
+    }
+}
---
+++ Items/weapons/SpikyHammer.cs
@@ -0,0 +1,35 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+
+namespace RaidersMod.Items.weapons
+{
+    class SpikyHammer : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Spiky Hammer");
+            Tooltip.SetDefault("Spiky Boi");
+        }
+        public override void SetDefaults()
+        {
+            item.noMelee = true;
+            item.noUseGraphic = true;
+            item.width = item.height = 16;
+            item.useStyle = 1;
+            item.useTime = 20;
+            item.useAnimation = 20;
+            item.shoot = ProjectileType<projectiles.SpikyHammerProj>();
+            item.shootSpeed = 16;
+            item.channel = true;
+            item.value = Item.buyPrice(0, 0, 50, 0);
+            item.damage = 35;
+            item.knockBack = 2f;
+        }
+        public override bool CanUseItem(Player player)
+        {
+            return player.ownedProjectileCounts[ProjectileType<projectiles.SpikyHammerProj>()] < 1;
+        }
+    }
+}
